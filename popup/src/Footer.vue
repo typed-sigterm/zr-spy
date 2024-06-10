0485a4fde4d779dae3dfb11cac1a225462b5b511
Typed SIGTERM <typed.sigterm@gmail.com>
2024-06-10 19:41:42 +0800

    chore: 更新一些信息

--- popup/src/Footer.vue
+++ popup/src/Footer.vue
@@ -6,11 +6,11 @@ import iconGitHub from './assets/github.svg?url'
 <template>
   <NText :depth="3">
     ZR spy ·
-    <NA href="https://github.com/Zihan-Hu/zr-spy/blob/main/CHANGELOG.md" target="_blank">
+    <NA href="https://github.com/typed-sigterm/zr-spy/blob/main/CHANGELOG.md" target="_blank">
       v{{ version }}
     </NA>
     ·
-    <NA href="https://github.com/Zihan-Hu/zr-spy" target="_blank">
+    <NA href="https://github.com/typed-sigterm/zr-spy" target="_blank">
       <img style="vertical-align: text-top;" :src="iconGitHub" alt="GitHub">
     </NA>
   </NText>
