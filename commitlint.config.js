39bc3910f8d097a3c4a996964979ab4800501b36
Typed SIGTERM <typed.sigterm@gmail.com>
2024-07-28 15:18:15 +0800

    refactor: 脚本不使用 TS

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,9 @@
-import type { UserConfig } from '@commitlint/types'
-
 const types = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']
 
+/** @type {import('@commitlint/types').UserConfig} */
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [...types, 'release']],
   },
-} satisfies UserConfig
+}
