aaa4aa47b491e7e9ce29b1c548da7e774af01024
Typed SIGTERM <typed.sigterm@gmail.com>
2024-07-28 15:29:36 +0800

    fix: import

--- entrypoints/popup/WithInitCaution.vue
+++ entrypoints/popup/WithInitCaution.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
-import { useStorageItem } from '~/utils/utils';
+import { resetRulesets, useStorageItem } from '~/utils/utils';
 
 defineSlots<{
   default: () => any
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,8 +1,13 @@
 import { ref, watch } from 'vue';
 import type { StorageItemKey, WxtStorageItemOptions } from 'wxt/storage';
+import { storage } from 'wxt/storage';
 
 export { version } from '~/package.json';
 
+/**
+ * 响应式获取 storage item。
+ * @see {@link storage.defineItem}
+ */
 export async function useStorageItem<T, D extends Record<string, unknown> = Record<string, unknown>>(key: StorageItemKey, options: WxtStorageItemOptions<T>) {
   const item = storage.defineItem<T, D>(key, options);
   const result = ref(await item.getValue());
